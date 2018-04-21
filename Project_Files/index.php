c18588cf001610e6212c2542dbf710b35661a304
colinxchen <colin.chen@colorado.edu>
2018-04-21 15:33:10 -0600

    added a very barebones right sidebar for entering and reading comments

--- Project_Files/index.php
+++ Project_Files/index.php
@@ -24,15 +24,9 @@
 
   <body id="index">
       <tr>
-        <td>
-          <div id="webtitle">
-            <h1>Snow Much Fun!</h1>
-          </div>
-        </td>
-        <td style="text-align: right;">
-          <div id="snowflake">
-            <img src="Images/SnowFlake.jpg" alt="SnowFlake" style="width:5%;height:5%;" >
-          </div>
+        <h1> Snow Much Fun!
+            <img align="right" src="Images/SnowFlake.jpg" alt="SnowFlake" style="width:4%;height:4%;" >
+	    </h1>
         </td>
 
       </tr>
@@ -50,10 +44,10 @@
   		<input type="password" placeholder="Password" name="password">
   	</div>
   	<div class="input-group">
-  		<button type="submit" class="btn" name="loginUser">Login</button>
+  		<button type="submit" class="btn btn-success mb-2" name="loginUser">Login</button>
   	</div>
-  	<p>
-      No account?<a href="UserRegistrationFrontend.php">Sign up Here</a>
+  	<p style="font-size:14px, text-align:center">
+      No account?<a href="UserRegistrationFrontend.php" style="font-size:18px">Sign up Here</a>
   	</p>
   </div>
   <div class="content">
@@ -74,7 +68,6 @@
     <?php endif ?>
   </div>
 </form>
-      <a href="./UserRegistrationFrontend.php" class="button">Sign up</a>
       <a href="./formActivity.html" class="button">Add Activities</a>
       <a href="./formWeather.html" class="button">Add Weather Conditions</a>
       <a href="./AboutUs.html" class="button">About</a>
@@ -86,9 +79,58 @@
       <input type="checkbox" name="Skiing" value="Skiing" onclick="checkSki()" id="Skiing"checked/><label for="Skiing"> Display Skiing</label><br>
       </div>
     </div>
+    
+<!--Right sidebar -->
+<div class="sidenav_right">
+
+<body>
+    <h2 align="center"><b>LOCATION NAME</b><br>
+    <img src="Images/climb.png" alt="climb icon" style="width:20%;height:20%;" >
+    </h2>
+    
+      <form style="margin-left: 20px">
+
+        <div style="font-size:0;height:10px;margin-top:0px;padding: 0px"></div>
+
+        <div style="font-size:0;height:0px;margin-top:0px;padding: 0px"></div>
+        <p style="margin-bottom: 0px"><b>Road Conditions</b></p>
+        <div class="form-group" style="width: 75%">
+          <input type="text" class="form-control" id="Road Conditions" placeholder="Describe road conditions to activity">
+        </div>
+      
+        <div style="font-size:0;height:10px;margin-top:0px;padding: 0px"></div>
+        <p style="margin-bottom: 0px"><b>Location Description</b></p>
+        <textarea class="form-control" id="LocationDescription" style="resize: vertical; max-width: 75%" rows="3"></textarea>
+        <div style="font-size:0;height:10px;margin-top:0px;padding: 0px"></div>
+
+        <div style="font-size:0;height:10px;margin-top:0px;padding: 0px"></div>
+        <p style="margin-bottom: 0px"><b>Activity Description</b></p>
+        <textarea class="form-control" id="ActivityDescription" style="resize: vertical; max-width: 75%" rows="3"></textarea>
+        <div style="font-size:0;height:10px;margin-top:0px;padding: 0px"></div>
+
+        <div class="form-inline">
+          <button type="submit" class="btn btn-success mb-2">Submit</button>
+        </div>
+      </form>
+<h3 align="center"><u>Comments</u></h3>
+<div class="comments">
+SomeUser-
+<p>
+All of his comments. <br>
+saying some random shit
+</p>
+</div>
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</div>
 
     <!-- Page content -->
     <div id="map"></div>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCQK0exnojgLgHApfdgjn6Km32cPxIXQZo&libraries=places&callback=initMap"></script>
+
+
   </body>
 </html>
